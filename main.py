97a3519bb466aa0163358ce1647e986ed91d25df
qooq12315 <128460076+qooq12315@users.noreply.github.com>
2023-06-27 06:46:17 +0000

    ok

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-
 from fastapi import FastAPI
-
+from datetime import datetime
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "banana"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+#query parameter
+@app.get("/raspberry")
+async def read_item(time:datetime = datetime.now(),light: float = 0.0, temperature: float = 0.0):
+    return {
+        "時間":time.strftime("%Y%m%d %H:%M:%S"),
+        "光線":light,
+        "溫度":temperature
+    }
